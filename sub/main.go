fd9a22d0aca8b1ff8fdfad40aab9aaa33e001a21
CJskii <czareczarek3@gmail.com>
2025-03-16 16:34:07 +0000

    refactor: websocket subscribtion + listening

--- sub/main.go
+++ sub/main.go
@@ -38,24 +38,22 @@ func main() {
 		log.Fatal("SECRET_KEY or ETH_HOST not set in environment.")
 	}
 
+	tokenString, err := decodeHexToBytes(secretKey)
+	if err != nil {
+		log.Fatalf("Failed to decode secret key: %v", err)
+	}
+	
+	conn, err := subscribeToWebsocket(string(tokenString), host)
+	if err != nil {
+		log.Fatalf("Failed to subscribe to websocket: %v", err)
+	}
+
+	subscribeToNewPendingTransactions(conn)	
+	listenToWebsocket(conn)
+}
+
+func subscribeToWebsocket(tokenString, host string) (*websocket.Conn, error) {
 	log.Printf("[ 🔌 ] Using Ethereum WebSocket host: %s", host)
-
-	// Decode the hex string into raw bytes.
-	secretBytes, err := hex.DecodeString(secretKey)
-	if err != nil {
-		log.Fatalf("Failed to decode secret hex: %v", err)
-	}
-
-	// Generate JWT token using HS256.
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"iat": time.Now().Unix(),
-		"exp": time.Now().Add(10 * time.Minute).Unix(),
-	})
-	tokenString, err := token.SignedString(secretBytes)
-	if err != nil {
-		log.Fatalf("Error generating JWT token: %v", err)
-	}
-
 	u := url.URL{Scheme: "ws", Host: host, Path: "/"}
 	log.Printf("[ 🔌 ] Connecting to WebSocket URL: %s", u.String())
 
@@ -68,11 +66,15 @@ func main() {
 
 	conn, _, err := dialer.Dial(u.String(), header)
 	if err != nil {
-		log.Fatalf("Error connecting to WebSocket: %v", err)
+		return nil, err
 	}
-	defer conn.Close()
+	log.Println("[ ✅ ] Connected to Ethereum WebSocket.")
 	log.Println("[ ✅ ] Connected to Ethereum WebSocket.")
 
+	return conn, nil
+}
+
+func subscribeToNewPendingTransactions(conn *websocket.Conn) {
 	subscribeRequest := RPCRequest{
 		Jsonrpc: "2.0",
 		ID:      1,
@@ -90,9 +92,10 @@ func main() {
 		log.Fatalf("Error reading subscription response: %v", err)
 	}
 	log.Printf("[ 👈 ] Received subscription response: %s", message)
+}
 
-	// Listen for new messages.
-	log.Println("[ 🔔 ] Starting to listen for pending transaction notifications...")
+func listenToWebsocket(conn *websocket.Conn) {
+	log.Println("[ 🔔 ] Listening to websocket...")
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
@@ -115,3 +118,22 @@ func main() {
 	}
 }
 
+func decodeHexToBytes(secretKey string) ([]byte, error) {
+	// Decode the hex string into raw bytes.
+	secretBytes, err := hex.DecodeString(secretKey)
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate JWT token using HS256.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iat": time.Now().Unix(),
+		"exp": time.Now().Add(10 * time.Minute).Unix(),
+	})
+	tokenString, err := token.SignedString(secretBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(tokenString), nil
+}
